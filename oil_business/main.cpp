f5b1453105788eab00ad061539ff1590c2c576b7
Arseniy Belkin <arseniibelkin@gmail.com>
2024-09-17 21:44:07 +0300

    Description of the properties of the pipe and compressor station

---
+++ oil_business/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+struct Pipe{
+    int name;
+    int length;
+    int diameter;
+    bool is_working;
+};
+
+
+struct CompressorStation{
+    string name;
+    int number_of_workshops;
+    int workshops_in_work;
+    float efficiency;
+};
+
+
+int main(){
+    return 0;
+}
