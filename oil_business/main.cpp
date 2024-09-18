609e678fbe3485af4a2578f48e4f1be29e0c7fec
Arseniy Belkin <arseniibelkin@gmail.com>
2024-09-18 20:28:53 +0300

    created func for objects, main menu and start fill that

--- oil_business/main.cpp
+++ oil_business/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 
 struct Pipe{
+    // труба
     int name;
     int length;
     int diameter;
@@ -13,6 +14,7 @@ struct Pipe{
 
 
 struct CompressorStation{
+    // компрессорная станция
     string name;
     int number_of_workshops;
     int workshops_in_work;
@@ -20,6 +22,132 @@ struct CompressorStation{
 };
 
 
+void Create(Pipe &pipe, int name, int lenght, int diameter, bool is_working){
+    // создать(переопределить свойства) трубу
+    pipe.name = name;
+    pipe.length = lenght;
+    pipe.diameter = diameter;
+    pipe.is_working = is_working;
+};
+
+
+void Create(CompressorStation &compressor_station, string name, int number_of_workshops, int workshops_in_work, float efficiency){
+    // создать(переопределить свойства) компрессорной станции
+    compressor_station.name = name;
+    compressor_station.number_of_workshops = number_of_workshops;
+    compressor_station.workshops_in_work = workshops_in_work;
+    compressor_station.efficiency = efficiency;
+};
+
+
+void print_main_menu(){
+    cout << "0 - exit\n";
+    cout << "1 - add pipe\n";
+    cout << "2 - add compressor station\n";
+    cout << "3 - see all objects\n";
+    cout << "4 - edit pipe\n";
+    cout << "5 - edit compressor stationn\n";
+    cout << "6 - save in file\n";
+    cout << "7 - upload from file\n";
+};
+
+
+void add_pipe(Pipe &pipe){
+    cout << "add pipe:\n";
+
+    cout << "name: ";
+    int name;
+    cin >> name;
+
+    cout << "lenght: ";
+    int lenght;
+    cin >> lenght;
+
+    cout << "diameter: ";
+    int diameter;
+    cin >> diameter;
+
+    cout << "is_working: ";
+    bool is_working;
+    cin >> is_working;
+
+    Create(pipe, name, lenght, diameter, is_working);
+
+    cout << "Pipe is created!\n";
+}
+
+
+void add_compressorStation(CompressorStation &compressor_station){
+    cout << "add compressor station:\n";
+
+    cout << "name: ";
+    string name;
+    cin >> name;
+
+    cout << "number of workshops: ";
+    int number_of_workshops;
+    cin >> number_of_workshops;
+
+    cout << "workshops in work: ";
+    int workshops_in_work;
+    cin >> workshops_in_work;
+
+    cout << "efficiency: ";
+    float efficiency;
+    cin >> efficiency;
+
+    Create(compressor_station, name, number_of_workshops, workshops_in_work, efficiency);
+
+    cout << "Comperssor Station is created!\n";
+}
+
+
+
+void main_menu(bool &flag, Pipe &pipe, CompressorStation &compressor_station){
+    print_main_menu();
+
+    int choice;
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 0:
+        flag = false;
+        cout << "Goodbye!\n";
+        break;
+    case 1:
+        add_pipe(pipe);
+        break;
+    case 2:
+        add_compressorStation(compressor_station);
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    case 7:
+        break;
+    default:
+        cout << "You choose the number, that not exist!\n";
+        break;
+    }
+    
+};
+
+
 int main(){
+    bool flag = true;
+    Pipe pipe = {};
+    CompressorStation compressor_station = {};
+
+
+    while (flag){
+        main_menu(flag, pipe, compressor_station);
+    };
+
     return 0;
 }
