a4b946c017e4a5d9afb60b8c845cd7f2890ff178
Arseniy Belkin <arseniibelkin@gmail.com>
2024-10-22 23:03:26 +0300

    start Pipe struct -> class

--- oil_business/header_files/pipe.h
+++ oil_business/header_files/pipe.h
@@ -1,21 +1,43 @@
-struct Pipe{
-    // труба
+#pragma once
+
+
+// struct Pipe{
+//     // труба
+//     std::string name;
+//     int length;
+//     int diameter;
+//     bool is_working;
+// }; +
+
+// bool is_created(const Pipe &pipe); +
+
+// std::string work_to_string(const Pipe &pipe); +
+
+// void edit_work_status(Pipe &pipe); +
+
+// std::ostream& operator << (std::ostream &os, const Pipe &pipe); +
+
+// void add_pipe(Pipe &pipe);
+
+// void save(std::ofstream &file, const Pipe &pipe); +
+
+// void load(std::ifstream &file, Pipe &pipe);
+
+
+class Pipe{
+private:
     std::string name;
     int length;
     int diameter;
     bool is_working;
+
+public:
+    Pipe();
+
+    bool is_created() const;
+    std::string work_to_string() const;
+    void edit_work_status();
+
+    friend std::ostream& operator << (std::ostream &os, const Pipe &pipe);
+    void save(std::ofstream &file);
 };
-
-bool is_created(const Pipe &pipe);
-
-std::string work_to_string(const Pipe &pipe);
-
-void edit_work_status(Pipe &pipe);
-
-std::ostream& operator << (std::ostream &os, const Pipe &pipe);
-
-void add_pipe(Pipe &pipe);
-
-void save(std::ofstream &file, const Pipe &pipe);
-
-void load(std::ifstream &file, Pipe &pipe);
--- oil_business/source_files/pipe.cpp
+++ oil_business/source_files/pipe.cpp
@@ -3,24 +3,25 @@
 #include <fstream>
 #include "../header_files/compressor_station.h"
 #include "../header_files/utils.h"
+#include <string>
 
 using namespace std;
 
 
-bool is_created(const Pipe &pipe){
-    return !(pipe.diameter == -1);
+bool Pipe::is_created() const {
+    return !(this->diameter == -1);
 }
 
 
-string work_to_string(const Pipe &pipe){
-    return pipe.is_working ? "yes" : "no";
+string Pipe::work_to_string() const{
+    return this->is_working ? "yes" : "no";
 }
 
 
-void edit_work_status(Pipe &pipe){
-    if (is_created(pipe)) {
+void Pipe::edit_work_status(){
+    if (this->is_created()) {
         cout << "New working status(0 - no / 1 - yes): ";
-        pipe.is_working = valid_int("New working status(0 - no / 1 - yes): ", 0, 1);
+        this->is_working = valid_int("New working status(0 - no / 1 - yes): ", 0, 1);
         cout << "New working status is set!" << endl;
     } else {
         cout << "The pipe has not been created yet!" << endl;
@@ -29,12 +30,12 @@ void edit_work_status(Pipe &pipe){
 
 
 ostream& operator << (ostream &os, const Pipe &pipe){
-    if (is_created(pipe)){
+    if (pipe.is_created()){
         os << "-----Pipe-----" << endl
             << "name: " << pipe.name << endl
             << "lenght: " << pipe.length << endl
             << "diameter: " << pipe.diameter << endl
-            << "is working: " << work_to_string(pipe) << endl
+            << "is working: " << pipe.work_to_string() << endl
             << "--------------" << endl;
     } else {
         os << "The pipe has not been created yet!" << endl;
@@ -44,39 +45,14 @@ ostream& operator << (ostream &os, const Pipe &pipe){
 }
 
 
-void add_pipe(Pipe &pipe){
-    if (is_created(pipe)){
-        cout << "Pipe is created already!\n";
-    } else {
-        cout << "-----add pipe-----\n";
-
-        cout << "name:";
-        cin.ignore(1000, '\n');
-        getline(cin, pipe.name);
-
-        cout << "lenght: ";
-        pipe.length = valid_int("lenght: ", 1, 10000);
-
-        cout << "diameter: ";
-        pipe.diameter = valid_int("diameter: ", 1, 1000);
-
-        cout << "is_working(0 - no / 1 - yes): ";
-        pipe.is_working = valid_int("is_working(0 - no / 1 - yes): ", 0, 1);;
-
-        cout << "Pipe is created!\n";
-
-        cout << "------------------\n";
-    };
-}
-
-
-void save(ofstream &file, const Pipe &pipe){
-    if (is_created(pipe)) {
+// TODO: change
+void Pipe::save(ofstream &file){
+    if (this->is_created()) {
         file << "Pipe" << endl;
-        file << pipe.name << endl;
-        file << pipe.length << endl;
-        file << pipe.diameter << endl;
-        file << pipe.is_working << endl;
+        file << this->name << endl;
+        file << this->length << endl;
+        file << this->diameter << endl;
+        file << this->is_working << endl;
         cout << "Pipe save in file!\n";
     } else {
         file << "None" << endl;
@@ -84,15 +60,93 @@ void save(ofstream &file, const Pipe &pipe){
     }
 }
 
-void load(ifstream &file, Pipe &pipe){
-    if (is_created(pipe)) {
-        cout << "Pipe is created already!\n";
-    } else {
-        file.ignore(1000, '\n');
-        getline(file, pipe.name);
-        file >> pipe.length;
-        file >> pipe.diameter;
-        file >> pipe.is_working;
-        cout << "Pipe is created from file!" << endl;
-    }
-}
+
+// bool is_created(const Pipe &pipe){
+//     return !(pipe.diameter == -1);
+// }
+
+
+// string work_to_string(const Pipe &pipe){
+//     return pipe.is_working ? "yes" : "no";
+// }
+
+
+// void edit_work_status(Pipe &pipe){
+//     if (is_created(pipe)) {
+//         cout << "New working status(0 - no / 1 - yes): ";
+//         pipe.is_working = valid_int("New working status(0 - no / 1 - yes): ", 0, 1);
+//         cout << "New working status is set!" << endl;
+//     } else {
+//         cout << "The pipe has not been created yet!" << endl;
+//     }
+// }
+
+
+// ostream& operator << (ostream &os, const Pipe &pipe){
+//     if (is_created(pipe)){
+//         os << "-----Pipe-----" << endl
+//             << "name: " << pipe.name << endl
+//             << "lenght: " << pipe.length << endl
+//             << "diameter: " << pipe.diameter << endl
+//             << "is working: " << work_to_string(pipe) << endl
+//             << "--------------" << endl;
+//     } else {
+//         os << "The pipe has not been created yet!" << endl;
+//     }
+
+//     return os;
+// }
+
+
+// void add_pipe(Pipe &pipe){
+//     if (is_created(pipe)){
+//         cout << "Pipe is created already!\n";
+//     } else {
+//         cout << "-----add pipe-----\n";
+
+//         cout << "name:";
+//         cin.ignore(1000, '\n');
+//         getline(cin, pipe.name);
+
+//         cout << "lenght: ";
+//         pipe.length = valid_int("lenght: ", 1, 10000);
+
+//         cout << "diameter: ";
+//         pipe.diameter = valid_int("diameter: ", 1, 1000);
+
+//         cout << "is_working(0 - no / 1 - yes): ";
+//         pipe.is_working = valid_int("is_working(0 - no / 1 - yes): ", 0, 1);;
+
+//         cout << "Pipe is created!\n";
+
+//         cout << "------------------\n";
+//     };
+// }
+
+
+// void save(ofstream &file, const Pipe &pipe){
+//     if (is_created(pipe)) {
+//         file << "Pipe" << endl;
+//         file << pipe.name << endl;
+//         file << pipe.length << endl;
+//         file << pipe.diameter << endl;
+//         file << pipe.is_working << endl;
+//         cout << "Pipe save in file!\n";
+//     } else {
+//         file << "None" << endl;
+//         cout << "The pipe has not been created yet!" << endl;
+//     }
+// }
+
+// void load(ifstream &file, Pipe &pipe){
+//     if (is_created(pipe)) {
+//         cout << "Pipe is created already!\n";
+//     } else {
+//         file.ignore(1000, '\n');
+//         getline(file, pipe.name);
+//         file >> pipe.length;
+//         file >> pipe.diameter;
+//         file >> pipe.is_working;
+//         cout << "Pipe is created from file!" << endl;
+//     }
+// }
